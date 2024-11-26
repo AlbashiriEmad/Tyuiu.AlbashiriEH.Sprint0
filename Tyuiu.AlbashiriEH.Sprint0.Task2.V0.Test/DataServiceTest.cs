2e561e3465f166554022386ea1325fd05cc5f13e
Emad Al-Bashiri <albashiriemad1@gmail.com>
2024-11-26 20:48:57 +0500

    Add project files.

---
+++ Tyuiu.AlbashiriEH.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.AlbashiriEH.Sprint0.Task2.V0.LIb;
+
+
+namespace Tyuiu.AlbashiriEH.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+
+        {
+            var name = "Эмад";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Эмад", res);
+        }
+    }
+}
---
+++ Tyuiu.AlbashiriEH.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.AlbashiriEH.Sprint0.Task2.V0.LIb;
+
+namespace Tyuiu.AlbashiriEH.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Эмад"));
+            Console.ReadKey();
+            Console.ReadKey();
+
+        }
+    }
+}
